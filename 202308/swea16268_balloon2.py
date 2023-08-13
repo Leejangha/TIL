ce66bdc0b5256444c36716df83bdf315e3dca24d
Leejangha <jangha0423@gmail.com>
2023-08-14 01:57:37 +0900

    0813

--- 202308/swea16268_balloon2.py
+++ 202308/swea16268_balloon2.py
@@ -22,3 +22,22 @@ for t in range(1, T+1):
                 max_val = s
 
     print(f'#{t} {max_val}')
+
+
+# 수정
+d = [(0,1), (1,0), (0,-1), (-1,0)]
+ 
+for t in range(int(input())):
+    N, M = map(int, input().split())
+    MAP = [list(map(int, input().split())) for _ in range(N)]
+    max_val = 0
+    for x in range(N):
+        for y in range(M):
+            ball = MAP[x][y]
+            for dx, dy in d:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < N and 0 <= ny < M:
+                    ball += MAP[nx][ny]
+            if ball > max_val:
+                max_val = ball
+    print(f'#{t+1} {max_val}')
