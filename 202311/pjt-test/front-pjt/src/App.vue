439392cef220668dfbac8b6f4680456e1d2b1897
Leejangha <jangha0423@gmail.com>
2023-11-17 18:00:43 +0900

    1117

--- 202311/pjt-test/front-pjt/src/App.vue
+++ 202311/pjt-test/front-pjt/src/App.vue
@@ -6,6 +6,27 @@
       <b-button variant="success">Button</b-button>
       <b-button variant="outline-primary">Button</b-button>
     </div>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>1 of 3</b-col>
+        <b-col>2 of 3</b-col>
+        <b-col>3 of 3</b-col>
+      </b-row>
+    </b-container>
+    <v-card title="Card title" subtitle="Subtitle" text="...">
+      <v-card-actions>
+        <v-btn>Click me</v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-list lines="one">
+      <v-list-item
+        v-for="n in 3"
+        :key="n"
+        :title="'Item ' + n"
+        subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+      ></v-list-item>
+    </v-list>
+
     <nav>
       <RouterLink :to="{ name: 'home' }">
         [Home]
@@ -26,6 +47,7 @@
 
 <script setup>
 import { RouterView, RouterLink } from 'vue-router'
+
 </script>
 
 <style scoped>
--- 202311/pjt-test/front-pjt/src/main.js
+++ 202311/pjt-test/front-pjt/src/main.js
@@ -8,11 +8,21 @@ import BootstrapVue3 from 'bootstrap-vue-3';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
+// // Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const app = createApp(App)
 const pinia = createPinia()
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
 
 app.use(BootstrapVue3)
+app.use(vuetify)
 app.use(pinia)
 app.use(router)
 
