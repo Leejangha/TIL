110d8539d94c304616ff8730adf2db91e2da99f7
Leejangha <jangha0423@gmail.com>
2024-01-20 19:06:50 +0900

    awfawfa2f

---
+++ react/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -0,0 +1,25 @@
+export default function GameBoard() {
+  return <ol id="game-board">
+    <li>
+      <ol>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ol>
+    </li>
+    <li>
+      <ol>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ol>
+    </li>
+    <li>
+      <ol>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ol>
+    </li>
+  </ol>
+}
